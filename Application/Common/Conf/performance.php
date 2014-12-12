b5b86fbbca7e2c2c42bf6f7454a2b7dbf0520ce4
duxweb <349865361>
2014-12-12 14:19:37 +0800

    更新伪静态规则

--- Application/Common/Conf/performance.php
+++ Application/Common/Conf/performance.php
@@ -4,6 +4,6 @@ return array(
     'TMPL_CACHE_ON'=>true, // 模板缓存
     'HTML_CACHE_ON'=>false, // 静态缓存
     'DB_SQL_BUILD_CACHE'=>false, // SQL查询缓存
-    'URL_ROUTER_ON'=>false, // URL路由
-    'URL_MODEL'=>3, // URL访问模式
+    'URL_ROUTER_ON'=>true, // URL路由
+    'URL_MODEL'=>2, // URL访问模式
 );
